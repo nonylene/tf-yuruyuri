bceae5f930df6993fd8964cbdb6b43ce2db83b0c
nonylene <cookie.speaker@gmail.com>
2016-06-29 23:19:56 +0900

    modify learning rate

--- cifar10.py
+++ cifar10.py
@@ -33,7 +33,7 @@ NUM_EXAMPLES_PER_EPOCH_FOR_EVAL = cifar10_input.NUM_EXAMPLES_PER_EPOCH_FOR_EVAL
 MOVING_AVERAGE_DECAY = 0.9999     # The decay to use for the moving average.
 NUM_EPOCHS_PER_DECAY = 350.0      # Epochs after which learning rate decays.
 LEARNING_RATE_DECAY_FACTOR = 0.1  # Learning rate decay factor.
-INITIAL_LEARNING_RATE = 1e-5       # Initial learning rate.
+INITIAL_LEARNING_RATE = 4e-4      # Initial learning rate.
 
 # If a model is trained with multiple GPUs, prefix all Op names with tower_name
 # to differentiate the operations. Note that this prefix is removed from the
